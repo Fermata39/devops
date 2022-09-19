4e2d7031d00f84fb89243276f3a69d144876dc9a
shinkook.kim <ksbang39@11stcorp.com>
2022-09-19 15:07:11 +0900

    create eks

--- Day1/Ch05/terraform/terraform-codes/route_table_association.tf
+++ Day1/Ch05/terraform/terraform-codes/route_table_association.tf
@@ -1,3 +1,4 @@
+# 라우트 테이블과 서브넷을 매핑하는 역할을 수행
 resource "aws_route_table_association" "test-route-association-pub-sub1" {
   route_table_id = aws_route_table.test-route-table-pub-sub1.id
   subnet_id      = aws_subnet.test-public-subnet1.id
--- Day1/Ch05/terraform/terraform-codes/security-group.tf
+++ Day1/Ch05/terraform/terraform-codes/security-group.tf
@@ -1,19 +1,21 @@
 
 resource "aws_security_group" "test-sg-eks-cluster" {
-  name        = "test-sg-eks-cluster"
-  description = "security_group for test-eks-cluster"
+  name        = "skt-user20-test-sg-eks-cluster"
+  description = "security_group for skt-user20-test-eks-cluster"
   vpc_id      = aws_vpc.test-vpc.id
 
   tags = {
-    Name = "test-sg-eks-cluster"
+    Name = "skt-user20-test-sg-eks-cluster"
   }
 }
 
+#port 0 은 any port
+#-1 프로토콜 상관 없이 지정할 때
 resource "aws_security_group_rule" "test-sg-eks-cluster-ingress" {
 
   security_group_id = aws_security_group.test-sg-eks-cluster.id
   type              = "ingress"
-  description       = "ingress security_group_rule for test-eks-cluster"
+  description       = "ingress security_group_rule for skt-user20-test-eks-cluster"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
@@ -24,7 +26,7 @@ resource "aws_security_group_rule" "test-sg-eks-cluster-egress" {
 
   security_group_id = aws_security_group.test-sg-eks-cluster.id
   type              = "egress"
-  description       = "egress security_group_rule for test-eks-cluster"
+  description       = "egress security_group_rule for skt-user20-test-eks-cluster"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
